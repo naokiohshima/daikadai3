ce35eea9849df08217885bb0be792e0d7c14acec
naoki ohshima <saylako@icloud.com>
2017-11-22 09:18:32 +0000

    seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,23 @@
-100.times do |n|
+2.times do |n|
   email = Faker::Internet.email
   uid = Faker::Commerce.department
   provider = Faker::Internet.email
   password = "password"
-  User.create!(email: email,
+  title = "title"
+  content = "content"
+  user = User.create!(email: email,
                password: password,
                password_confirmation: password,
                uid: uid,
                provider: provider,
-               avatar: File.open("./public/l_e_others_500.png")  
+               avatar: File.open("./public/l_e_others_500.png")
+             )
+  blog = Blog.create!(title: title,
+               content: content,
+               user_id: user.id
+             )
+  Comment.create!(content: content,
+                  user_id: user.id,
+                  blog_id: blog.id
              )
 end
